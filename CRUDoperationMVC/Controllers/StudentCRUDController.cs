ceece85c6dcf9e795df098df5222f8c6bc7a050f
VijayWMP <vijayy@whatsmyproduct.com>
2019-05-08 16:59:31 +0530

    commit

---
+++ CRUDoperationMVC/Controllers/StudentCRUDController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUDoperationMVC.Entity;
+
+namespace CRUDoperationMVC.Controllers
+{
+    public class StudentCRUDController : Controller
+    {
+        private EFCrudEntities db = new EFCrudEntities();
+
+        // GET: StudentCRUD
+        public ActionResult Index()
+        {
+            return View(db.StudentInfoes.ToList());
+        }
+
+        // GET: StudentCRUD/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentInfo studentInfo = db.StudentInfoes.Find(id);
+            if (studentInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentInfo);
+        }
+
+        // GET: StudentCRUD/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentCRUD/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StudentId,Course,Name")] StudentInfo studentInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.StudentInfoes.Add(studentInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(studentInfo);
+        }
+
+        // GET: StudentCRUD/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentInfo studentInfo = db.StudentInfoes.Find(id);
+            if (studentInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentInfo);
+        }
+
+        // POST: StudentCRUD/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StudentId,Course,Name")] StudentInfo studentInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(studentInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(studentInfo);
+        }
+
+        // GET: StudentCRUD/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentInfo studentInfo = db.StudentInfoes.Find(id);
+            if (studentInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentInfo);
+        }
+
+        // POST: StudentCRUD/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            StudentInfo studentInfo = db.StudentInfoes.Find(id);
+            db.StudentInfoes.Remove(studentInfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CRUDoperationMVC/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CRUDoperationMVC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("CRUDoperationMVC")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("93990d12-bb9e-4458-86ac-ec8049bc9e5c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
